c2dcefc7f4990b943975e375debb5090dabd8bca
smallmocha <34787633+smallmocha@users.noreply.github.com>
2020-06-04 14:47:30 +0800

    Create test.js

---
+++ assets/test.js
@@ -0,0 +1,1 @@
+!function(a,b){"object"==typeof module&&"object"==typeof module.exports?module.exports=a.document?b(a,!0):function(a){if(!a.document)throw new Error("jQuery requires a window with a document");
