a33f84427f9d2862dc82f30644eb59e7a6852dcd
smallmocha <34787633+smallmocha@users.noreply.github.com>
2020-06-17 10:52:55 +0800

    Delete test.js

--- assets/test.js
+++
@@ -1,1 +0,0 @@
-!function(a,b){"object"==typeof module&&"object"==typeof module.exports?module.exports=a.document?b(a,!0):function(a){if(!a.document)throw new Error("jQuery requires a window with a document");
